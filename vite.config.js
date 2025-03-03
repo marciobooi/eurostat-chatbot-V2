25c29707757901915e193e610f272b31430e9217
marcio <78marcio@gmail.com>
2025-03-03 18:34:56 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // Use the plugin you have installed
 
 // https://vitejs.dev/config/
 export default defineConfig({
