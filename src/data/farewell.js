45b57d946845a23b677d8365609eb9dffc9174fc
marcio <78marcio@gmail.com>
2025-06-18 22:46:46 +0200

    feat: Enhance greeting and farewell functionalities by expanding greeting words and adding farewell responses; improve user interaction

---
+++ src/data/farewell.js
@@ -0,0 +1,41 @@
+
+export const goodbyeWords = {
+    en: [
+      "Goodbye",
+      "See you",
+      "Farewell",
+      "Bye",
+      "Later",
+      "So long",
+      "Take care",
+      "Cheerio",
+      "Adios",
+      "Bye bye",
+      "Catch you",
+      "Peace out",
+      "Ta-ta",
+      "Hasta la vista",
+      "See ya",
+      "Toodle-oo",
+      "Ciao",
+      "Sayonara",
+      "Cheers",
+      "Till then",
+    ],  
+  };
+  
+
+  export const FAREWELL_RESPONSES = {
+  en: [
+    "Goodbye! Feel free to come back if you have more questions.",
+    "Thanks for chatting! Have a great day.",
+    "See you next time! Feel free to return if you need more information.",
+    "Farewell! Don't hesitate to ask more energy questions later.",
+    "Thank you for using our service. Goodbye!",
+    "Take care! I'm here whenever you need assistance.",
+    "Have a great day! Come back anytime.",
+    "It was a pleasure chatting with you. See you soon!",
+    "Goodbye for now! Let me know if you ever need help.",
+    "Wishing you all the best! See you next time.",
+  ]
+}
--- src/utils/intentMessages.js
+++ src/utils/intentMessages.js
@@ -2,9 +2,16 @@
  * message flow for the chat
  * 
  * This implements a comprehensive intent flow processing for the messages 
+ * we try to find the word in this dictionaries inside the data folder
+ * so better add tokkenization
  * 
  * messages Flow:
- * 1. welcome
- * 2. find definition (ruler)
+ * 1. User Input → Trim + Lowercase
+ * 2. Check spelling with nspell
+ * 3. greetings
+ * 4. find definition (this will be delegate to the ruler we just pass the query)
+ * 5. farewell
 
+
+    all except the point 2 definitions we should provide a ramdom answer
  */
